9d36213ef6903babb048c40f7600998961ce34bc
Marios Dallas <mariosdallas2995@gmail.com>
2025-09-26 22:54:48 +0200

    Camera
    Added Camera class with proper WASD and mouse control movement

--- Window.h
+++ Window.h
@@ -19,6 +19,10 @@ public:
 
 	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
 
+	bool* getKeys() { return keys; }
+	GLfloat getXchange();
+	GLfloat getYchange();
+
 	void swapBuffers() { glfwSwapBuffers(mainWindow); }
 
 	~Window();
@@ -28,4 +32,17 @@ private:
 
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+
+
+	bool keys[1024]; // covers the range of ascii characters, all keys have a number to be adressed and tracked
+
+	GLfloat lastX; // every time the mouse moves we compare to the last coords
+	GLfloat lastY;
+	GLfloat xChange; // comparison of starting to last coords calculate the change
+	GLfloat yChange;
+	bool mouseFirstMoved; // takes into account the first initial movement
+
+	void createCallbacks();
+	static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode); // we need static because glfw will do a callback 
+	static void handleMouse(GLFWwindow* window, double xPos, double yPos);
 };
